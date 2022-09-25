cdf33815778f3e8fa7384aecaf647bff238166e3
fatec <fatec@fatec.com>
2022-09-25 17:55:19 -0300

    v0.3 #nav fixo

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function exibirEquipe(){
     n = equipe.length-1;
     for(var i = 0; i <= n; i++){
         document.write(
-            "<div class='col-lg-4 px-5 mt-5'>" +
+            "<div class='col-lg-4 px-3 mt-5 mb-5'>" +
                 "<img src='imgs/equipe/"+ equipe[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue + "' alt='' class='bd-placeholder-img rounded-circle' width='160' height='160'>" +
                 "<h2 class='eqp_h2 mt-3 mb-3'>" + equipe[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue + "</h2>" +
                 "<p class='eqp_p'>" + equipe[i].getElementsByTagName("frase")[0].childNodes[0].nodeValue + "</p>" +
